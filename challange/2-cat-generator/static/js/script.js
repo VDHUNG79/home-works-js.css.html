c35e154194c26e717012bef29f8d059ffd3ecb5a
VDHUNG79 <64667768+VDHUNG79@users.noreply.github.com>
2020-08-14 23:40:16 +0700

    cat-love

---
+++ challange/2-cat-generator/static/js/script.js
@@ -0,0 +1,26 @@
+// alert('Hi');
+// console.log('Hello');
+
+// Challenge 1: Your age in Days
+
+function ageInDays() {
+    var birthYear = prompt('What year were you born ... Goood friend?');
+    var ageInDayss = (2020 - birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+}
+
+// Challenge 2: Cat Generator
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "https://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
+}
