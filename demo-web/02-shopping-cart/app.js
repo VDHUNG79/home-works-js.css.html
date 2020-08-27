48768e5e6239de6cba5819b61a0588c983ff3910
VDHUNG79 <64667768+VDHUNG79@users.noreply.github.com>
2020-08-28 01:38:49 +0700

    Enable & Disable Add Buttons

--- demo-web/02-shopping-cart/app.js
+++ demo-web/02-shopping-cart/app.js
@@ -10,6 +10,7 @@ const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
 
+
 // cart
 let cart = [];
 
@@ -57,11 +58,29 @@ class UI {
         })
         productsDOM.innerHTML = result;
     }
+    getBagButtons() {
+        const buttons = [...document.querySelectorAll('.bag-btn')];
+        buttons.forEach(button => {
+            let id = button.dataset.id;
+            let inCart = cart.find(item => item.id === id);
+            if (inCart) {
+                button.innerText = "In Cart";
+                buttons.disabled = true;
+            } else {
+                button.addEventListener('click', event => {
+                    event.target.innerText = "In Cart";
+                    event.target.disabled = true;
+                });
+            }
+        });
+    }
 }
 
 //  local storage
 class Storage {
-
+    static saveProducts(products) {
+        localStorage.setItem("products", JSON.stringify(products));
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -69,5 +88,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const products = new Products();
 
     //  get all products
-    products.getProducts().then(products => ui.displayProducts(products));
+    products.getProducts().then(products => { 
+        ui.displayProducts(products)
+    Storage.saveProducts(products);
+    }).then(() => {
+        ui.getBagButtons();
+    });
 })
