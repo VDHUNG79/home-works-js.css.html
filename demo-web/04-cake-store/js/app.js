cf854035b7eca6f092a104a94b77fe76ee980a2b
VDHUNG79 <64667768+VDHUNG79@users.noreply.github.com>
2020-09-29 23:56:40 +0700

    web layout

--- demo-web/04-cake-store/js/app.js
+++ demo-web/04-cake-store/js/app.js
@@ -1,5 +1,27 @@
-document.getElementById('cart-info').addEventListener('click', function () {
+// document.getElementById('cart-info').addEventListener('click', function () {
+//     const cart = document.getElementById('cart');
+//     cart.classList.toggle('show-cart');
+//     console.log(cart);
+// });
+
+// show cart
+(function () {
+    const cartInfo = document.getElementById('cart-info');
     const cart = document.getElementById('cart');
-    cart.classList.toggle('show-cart');
-    console.log(cart);
-});
+
+    cartInfo.addEventListener('click', function () {
+        cart.classList.toggle('show-cart');
+    });
+})();
+
+// add item to the cart
+(function () {
+    const cartBtn = document.querySelectorAll('.store-item-icon');
+
+    cartBtn.forEach(function (btn) {
+        btn.addEventListener('click', function (event) {
+
+            console.log(event.target);
+        })
+    })
+})();
