1336ee6a3a0ab736ee826bc6a5f4188c669c0a2f
VDHUNG79 <64667768+VDHUNG79@users.noreply.github.com>
2020-10-04 22:53:59 +0700

    add parallax effect

--- demo-web/05 - Responsive Website/js/parallax.js
+++ demo-web/05 - Responsive Website/js/parallax.js
@@ -1,15 +1,27 @@
-$(window).scroll(function() {
+// $(window).scroll(function() {
+//   parallax();
+// })
+
+// function parallax() {
+
+//   var wScroll = $(window).scrollTop()
+
+
+//   $('.parallax--bg').css('background-position', 'center ' + (wScroll*0.75)+'px');
+
+
+//   $('.parallax--box').css('top', -5 + (wScroll*.005)+'em')
+
+// }
+
+$(window).scroll(function () {
   parallax();
 })
 
 function parallax() {
-
-  var wScroll = $(window).scrollTop()
-
-
-  $('.parallax--bg').css('background-position', 'center ' + (wScroll*0.75)+'px');
-
-
-  $('.parallax--box').css('top', -5 + (wScroll*.005)+'em')
-
+  var wScroll = $(window).scrollTop();
+  // console.log(wScroll);
+  $('.parallax--bg').css('background-position', 'center ' + (wScroll * 0.75) + 'px');
+  
+  $('.parallax--box').css('top', -5+(wScroll*0.005) + 'em');
 }
