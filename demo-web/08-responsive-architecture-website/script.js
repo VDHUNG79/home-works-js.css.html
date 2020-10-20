da7199d6fa37593c9d9fb8a0c719888a31f96caa
VDHUNG79 <64667768+VDHUNG79@users.noreply.github.com>
2020-10-20 09:05:56 +0700

    add spinnerm

--- demo-web/08-responsive-architecture-website/script.js
+++ demo-web/08-responsive-architecture-website/script.js
@@ -1,6 +1,17 @@
-document.querySelector('.hamburger-menu');
+window.onload = () => {
+    setTimeout(() => {
+        document.querySelector("body").classList.add("display");
+    }, 4000);
+};
 
+document.querySelector(".hamburger-menu");
 addEventListener('click', () => {
-    document.querySelector('.container').classList.toggle("change");
+    document.querySelector(".container").classList.toggle("change");
+});
 
+document.querySelector('.scroll-btn').addEventListener('click', () => {
+    document.querySelector('html').style.scrollBehavior = "smooth";
+    setTimeout(() => {
+        document.querySelector('html').style.scrollBehavior = "unset";
+    }, 1000);
 });
