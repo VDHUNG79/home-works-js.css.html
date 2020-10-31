ec8c028f0d35f351b8e36ce4eadf4ba5a4ac86d3
VDHUNG79 <64667768+VDHUNG79@users.noreply.github.com>
2020-10-31 19:03:06 +0700

    practice some css

--- demo-web/09-ecommerce-php/index.js
+++ demo-web/09-ecommerce-php/index.js
@@ -76,10 +76,11 @@ $(document).ready(function () {
     //  Product Qty Section
     let $qty_up = $(".qty .qty-up");
     let $qty_down = $(".qty .qty-down");
-    let $input = $(".qty .qty_input");
+    // let $input = $(".qty .qty_input");
 
     // click on qty up button
     $qty_up.click(function (e) {
+        let $input = $(`.qty_input[data-id = '${$(this).data("id")}']`);
         if ($input.val() >= 1 && $input.val() <= 9) {
             $input.val(function (i, oldval) {
                 return ++oldval;
@@ -88,13 +89,14 @@ $(document).ready(function () {
     });
 
         // click on qty down button
-        $qty_down.click(function (e) {
-            if ($input.val() > 1 && $input.val() <= 10) {
-                $input.val(function (i, oldval) {
-                    return --oldval;
-                });
-            }
-        });
+    $qty_down.click(function (e) {
+        let $input = $(`.qty_input[data-id = '${$(this).data("id")}']`);
+        if ($input.val() > 1 && $input.val() <= 10) {
+            $input.val(function (i, oldval) {
+                return --oldval;
+            });
+        }
+    });
 
 
 
