27ff47b08302e254f2870aa915ad97508af22b72
VDHUNG79 <64667768+VDHUNG79@users.noreply.github.com>
2020-11-09 02:20:41 +0700

    Blog Content

--- demo-web/10-blogger-website/js/main.js
+++ demo-web/10-blogger-website/js/main.js
@@ -7,4 +7,17 @@ $(document).ready(function () {
     $toogleCollapse.click(function () {
         $nav.toggleClass("collapse"); // Class collapse is not create yet. Go into main.css and style .collapse
     });
+
+    // Owl Carousel for Blog
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+
+        // Change to arrow icons
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+    });
+
 });
