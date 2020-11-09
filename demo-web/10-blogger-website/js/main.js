9b3c365de86fc3d9f7cc75bc2cf044ff9030960b
VDHUNG79 <64667768+VDHUNG79@users.noreply.github.com>
2020-11-09 11:39:41 +0700

    create some animation

--- demo-web/10-blogger-website/js/main.js
+++ demo-web/10-blogger-website/js/main.js
@@ -1,3 +1,20 @@
+// responsive for carousel 03:02:00
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    }, 
+    560: {
+        items: 2
+    }, 
+    960: {
+        items: 3
+    }
+}
+
+
 // Toogle Bars 01:02:00
 $(document).ready(function () {
     $nav = $(".nav");
@@ -17,7 +34,20 @@ $(document).ready(function () {
         nav: true,
 
         // Change to arrow icons
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+
+        // responsive for carousel 03:02:00
+        responsive: responsive
     });
+
+    // Click to scroll top 02:58:00
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+    // AOS Animation
+    AOS.init();
 
 });
