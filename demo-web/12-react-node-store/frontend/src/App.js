910602ff3977aaab1199091e28ca16cdb34a26b8
VDHUNG79 <64667768+VDHUNG79@users.noreply.github.com>
2020-12-08 00:45:08 +0700

    delete error

---
+++ demo-web/12-react-node-store/frontend/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">amazon</a>
+          </div>
+
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+
+        <main>
+          <Route path="/product/:id" component={ProductScreen} ></Route>
+
+          <Route path="/" component={HomeScreen} exact></Route>
+          
+        </main>
+
+        <footer className="row center">All rights reserved</footer>
+      </div>
+    </BrowserRouter>
+      
+  );
+}
+
+export default App;
